cb46b1e33079494173edb650b53d72ecc437280f
franciscoRdiaz <francisco.diazbayon@gmail.com>
2017-06-01 11:02:20 +0000

    Modify docker configuration.

--- docker/elastest-torm/testing-mysqlstate-entrypoint.sh
+++ docker/elastest-torm/testing-mysqlstate-entrypoint.sh
@@ -3,4 +3,4 @@ while ! nc -z mysql 3306 ; do
     echo "Waiting for upcoming Config Server"
     sleep 2
 done
-exec java -jar ../../elastest-torm/target/elastest-torm-0.0.1-SNAPSHOT.jar
+exec java -jar elastest-torm-0.0.1-SNAPSHOT.jar
