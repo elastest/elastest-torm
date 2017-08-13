8ce719ea6fb64faa24d0e8c4ef3c057948528de6
Micael Gallego <micael.gallego@gmail.com>
2017-08-13 19:57:47 +0200

    Increase WebSocket message max size to avoid disconnection in tests

--- elastest-torm/src/main/java/io/elastest/etm/ElastestETMSpringBoot.java
+++ elastest-torm/src/main/java/io/elastest/etm/ElastestETMSpringBoot.java
@@ -2,18 +2,16 @@ package io.elastest.etm;
 
 import java.util.concurrent.Executor;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import io.elastest.etm.utils.UtilTools;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,54 +19,44 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableAsync
-@ComponentScan(basePackages = "io.elastest.etm")
-public class ElastestETMSpringBoot extends AsyncConfigurerSupport  implements CommandLineRunner {
+public class ElastestETMSpringBoot extends AsyncConfigurerSupport {
 
-	@Override
-	public void run(String... arg0) throws Exception {
-		if (arg0.length > 0 && arg0[0].equals("exitcode")) {
-			throw new ExitException();
-		}
-	}
-
-	public static void main(String[] args) throws Exception {
-		new SpringApplication(ElastestETMSpringBoot.class).run(args);
-	}
-
-	class ExitException extends RuntimeException implements ExitCodeGenerator {
-		private static final long serialVersionUID = 1L;
-
-		@Override
-		public int getExitCode() {
-			return 10;
-		}
-	}
-	
 	@Bean
-	UtilTools utils(){
+	UtilTools utils() {
 		UtilTools utils = new UtilTools();
 		return utils;
 	}
-	
+
 	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**").allowedMethods("*").allowedOrigins("*");
-            }
-        };
-    }
-	
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
+	}
+
 	@Override
-    public Executor getAsyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(4);
-        executor.setMaxPoolSize(6);
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("TJob-");
-        executor.initialize();
-        return executor;
-    }
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(4);
+		executor.setMaxPoolSize(6);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("TJob-");
+		executor.initialize();
+		return executor;
+	}
+
+	@Bean
+	public ServletServerContainerFactoryBean createWebSocketContainer() {
+		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+		container.setMaxTextMessageBufferSize(65500);
+		container.setMaxBinaryMessageBufferSize(65500);
+		return container;
+	}
 	
+	public static void main(String[] args) throws Exception {
+		new SpringApplication(ElastestETMSpringBoot.class).run(args);
+	}
 }
--- elastest-torm/src/test/java/io/elastest/etm/test/api/TJobExecutionApiItTest.java
+++ elastest-torm/src/test/java/io/elastest/etm/test/api/TJobExecutionApiItTest.java
@@ -13,6 +13,9 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import javax.websocket.ContainerProvider;
+import javax.websocket.WebSocketContainer;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,11 +31,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
@@ -41,6 +46,7 @@ import org.springframework.web.socket.messaging.WebSocketStompClient;
 import io.elastest.etm.ElastestETMSpringBoot;
 import io.elastest.etm.api.model.TJob;
 import io.elastest.etm.api.model.TJobExecution;
+import io.elastest.etm.api.model.TJobExecution.ResultEnum;
 
 @RunWith(JUnitPlatform.class)
 @ExtendWith(SpringExtension.class)
@@ -57,27 +63,51 @@ public class TJobExecutionApiItTest extends EtmApiItTest {
 		}
 
 		@Override
-		public void handleFrame(StompHeaders stompHeaders, Object o) {
-			log.info("handleFrame.");
-			log.info((String) o);
-			if (((String) o).contains("BUILD SUCCESS") || ((String) o).contains("BUILD FAILURE")) {
+		public void handleFrame(StompHeaders stompHeaders, Object msg) {
+			
+			log.info("Stomp message: "+ msg);
+			
+			String strMsg = (String) msg;			
+			if (strMsg.contains("BUILD SUCCESS") || strMsg.contains("BUILD FAILURE")) {
 				log.info("Match message");
 				latch.countDown();
 			}
 		}
 
 		public void waitForCompletion() throws InterruptedException {
-			int timeSeconds = 60;
-			if(!latch.await(timeSeconds, TimeUnit.SECONDS)){
-				throw new RuntimeException("Timeout of "+timeSeconds+" waiting for 'BUILD SUCESS' message");
+			int timeSeconds = 120;
+			if (!latch.await(timeSeconds, TimeUnit.SECONDS)) {
+				throw new RuntimeException("Timeout of " + timeSeconds + " waiting for 'BUILD SUCESS' message");
 			}
 		}
 	}
 
-	private class LogConnectedSessionHandler extends StompSessionHandlerAdapter {
+	private class LogConnectedSessionHandler implements StompSessionHandler {
 		public void afterConnected(StompSession stompSession, StompHeaders stompHeaders) {
 			log.info("STOMP Client connected");
 		}
+
+		@Override
+		public Type getPayloadType(StompHeaders headers) {
+			log.info("getPayloadType");
+			return null;
+		}
+
+		@Override
+		public void handleFrame(StompHeaders headers, Object payload) {
+			log.info("handleFrame: "+payload);			
+		}
+
+		@Override
+		public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload,
+				Throwable exception) {
+			log.info("handleException: StopmSession:"+session+" StompCommand:"+command+" Exception:"+exception);			
+		}
+
+		@Override
+		public void handleTransportError(StompSession session, Throwable exception) {
+			log.info("handleTransportError: StopmSession:"+session+" Exception:"+exception);			
+		}		
 	}
 
 	private static final Logger log = LoggerFactory.getLogger(TJobExecutionApiItTest.class);
@@ -101,51 +131,77 @@ public class TJobExecutionApiItTest extends EtmApiItTest {
 		log.info("Start the test testExecuteTJob");
 
 		TJob tJob = createTJob(projectId);
-		
-		WebSocketClient webSocketClient = new StandardWebSocketClient();
-		WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
-		stompClient.setMessageConverter(new StringMessageConverter());
 
-		String url = "ws://localhost:" + serverPort + "/rabbitMq";
-		StompSessionHandler sessionHandler = new LogConnectedSessionHandler();
-		StompSession stompSession = stompClient.connect(url, sessionHandler).get(10, TimeUnit.SECONDS);
+		StompSession stompSession = connectToRabbitMQ();
 
 		log.info("POST /api/tjob/{tjobId}/exec");
-		
+
 		Map<String, Object> urlParams = new HashMap<>();
 		urlParams.put("tjobId", tJob.getId());
-		
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 
 		String body = "[]";
 		HttpEntity<String> entity = new HttpEntity<>(body, headers);
-		
+
 		ResponseEntity<TJobExecution> response = httpClient.postForEntity("/api/tjob/{tjobId}/exec", entity,
 				TJobExecution.class, urlParams);
-		
+
 		TJobExecution exec = response.getBody();
-		
+
 		log.info("TJobExecution creation response: " + response);
 
 		String queueToSuscribe = "/topic/" + "test." + response.getBody().getId() + ".log";
-		log.info("TJob log queue '"+queueToSuscribe+"'");
-		
+		log.info("TJob log queue '" + queueToSuscribe + "'");
+
 		WaitForFinishHandler handler = new WaitForFinishHandler();
 		stompSession.subscribe(queueToSuscribe, handler);
 
 		handler.waitForCompletion();
 
-		assertAll("Validating TJobExecution Properties", 
-				() -> assertNotNull(response.getBody()),
+		assertAll("Validating TJobExecution Properties", () -> assertNotNull(response.getBody()),
 				() -> assertNotNull(response.getBody().getId()),
 				() -> assertTrue(response.getBody().getTjob().getId().equals(urlParams.get("tjobId"))));
 
-		log.info("TJobExecution: "+getTJobExecutionById(exec.getId(), tJob.getId()));
-		
+		while (true) {
+			exec = getTJobExecutionById(exec.getId(), tJob.getId()).getBody();
+			log.info("TJobExecution: " + exec);
+			if(exec.getResult() != ResultEnum.IN_PROGRESS ){
+				break;
+			}
+			sleep(500);
+		}
+
 		deleteTJobExecution(exec.getId(), tJob.getId());
 		deleteTJob(tJob.getId());
 		log.info("Finished.");
+	}
+
+	private StompSession connectToRabbitMQ() throws InterruptedException, ExecutionException, TimeoutException {
+		WebSocketContainer cont = ContainerProvider.getWebSocketContainer();
+		cont.setDefaultMaxTextMessageBufferSize(65500);
+		WebSocketClient webSocketClient = new StandardWebSocketClient(cont);
+		
+		WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
+		stompClient.setMessageConverter(new StringMessageConverter());
+		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+		taskScheduler.initialize();
+		stompClient.setTaskScheduler(taskScheduler); // for heartbeats
+		stompClient.setDefaultHeartbeat(new long[]{10000,10000});
+
+		String url = "ws://localhost:" + serverPort + "/rabbitMq";
+		StompSessionHandler sessionHandler = new LogConnectedSessionHandler();
+		StompSession stompSession = stompClient.connect(url, sessionHandler).get(10, TimeUnit.SECONDS);
+		return stompSession;
+	}
+
+	private void sleep(int waitTime) {
+		try {
+			Thread.sleep(waitTime);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 	}
 
 }
