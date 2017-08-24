11864696c64a1737aca5d9b605463b8d2a5fa679
Micael Gallego <micael.gallego@gmail.com>
2017-08-24 16:45:42 +0200

    Add codecov coverage upload to it.sh

--- scripts/it.sh
+++ scripts/it.sh
@@ -51,8 +51,14 @@ echo "Starting maven integration tests"
 cd ../elastest-torm
 
 mvn -B -Dspring.datasource.url=jdbc:mysql://${MYSQL_IP}:3306/elastest-etm?useSSL=false -Dspring.rabbitmq.host=${RABBIT_IP} -Delastest.elasticsearch.host=http://${ELASTICSEARCH_IP}:9200/ -Dlogstash.host=${LOGSTASH_IP} -Delastest.incontainer=true clean verify
-            
+
 mvnExit=$?
+
+# Generate test coverage report
+mvn -B jacoco:report
+
+# Upload coverage report to codecov.io
+bash <(curl -s https://codecov.io/bash) -t fa48b15c-ceb8-409d-996f-8f34d53addd2
 
 # Clean up environment
 
