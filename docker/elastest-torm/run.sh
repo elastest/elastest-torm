7b5714b82e6c105a8ecf0e57aca6becc749e0ffc
edujgurjc <ejg.urjc@gmail.com>
2017-09-13 09:03:47 +0200

    run.sh modified to pass EXEC_MODE env var

--- docker/elastest-torm/run.sh
+++ docker/elastest-torm/run.sh
@@ -3,4 +3,11 @@ while ! nc -z mysql 3306 ; do
     echo "MySQL server in not ready in address 'mysql' and port 3306"
     sleep 2
 done
-exec java -jar elastest-torm.jar
+
+if [ -z "$EXEC_MODE" ]; then
+  echo "Execution Mode: Lite"
+  exec java -jar elastest-torm.jar
+else
+  echo "Execution Mode: " $EXEC_MODE
+  exec java -jar elastest-torm.jar --elastest.execution.mode=$EXEC_MODE
+fi
