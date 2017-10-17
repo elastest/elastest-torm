cfe8bbb720068b12086d941ca207b9d46a7d7588
edujgurjc <ejg.urjc@gmail.com>
2017-10-17 09:56:54 +0200

    GUI => elasticsearch prod changed to localhost

--- elastest-torm-gui/src/environments/environment.prod.ts
+++ elastest-torm-gui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment: { production: boolean, hostElasticSearch: string, eus: string } = {
   production: true,
-  hostElasticSearch: window.location.hostname + ':9200',
+  hostElasticSearch: 'localhost:9200',
   eus: window.location.hostname + ':8040'
 };
