bf48f8ca3e20c5662a4ceabd7bb79b0f240a05d9
franciscoRdiaz <francisco.diazbayon@gmail.com>
2017-11-09 12:52:02 +0100

    feat(basic-authentication): Set optional the basic-athentication

--- docker/services/nginx/start-nginx.sh
+++ docker/services/nginx/start-nginx.sh
@@ -1,8 +1,10 @@
 #!bin/bash
 
 if [[ -z "${ET_SECURITY}" ]]; then
-   echo 'Nginx without security'
+	echo 'Nginx without basic authentication'
+	rm /etc/nginx/conf.d/nginx-securized.conf
 else
+	echo 'Nginx with basic authentication'
     htpasswd -b -c /etc/nginx/conf.d/nginx.htpasswd $ET_USER $ET_PASS
     mv /etc/nginx/conf.d/nginx-securized.conf /etc/nginx/conf.d/default.conf
 fi
