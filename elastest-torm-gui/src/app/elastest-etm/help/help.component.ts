70495ad33b490454f87f2d294c73e3ef18e0b182
edujgurjc <ejg.urjc@gmail.com>
2017-12-18 16:48:21 +0100

    gui => show version into help

--- elastest-torm-gui/src/app/elastest-etm/help/help.component.ts
+++ elastest-torm-gui/src/app/elastest-etm/help/help.component.ts
@@ -1,6 +1,7 @@
 import { TitlesService } from '../../shared/services/titles.service';
 import { ConfigurationService } from '../../config/configuration-service.service';
 import { Component, OnInit } from '@angular/core';
+import { version } from 'punycode';
 
 @Component({
   selector: 'etm-help',
@@ -11,18 +12,52 @@ export class HelpComponent implements OnInit {
 
   versionsInfo: any[];
   elastestModulesNames: string[];
+  versionsInfoDataTable: any[] = [];
+
+  etCurrentVersion: string;
+
+  // SuT Data
+  versionsInfoColumns: any[] = [
+    { name: 'imageName', label: 'Image Name' },
+    { name: 'date', label: 'Date' },
+    { name: 'commitId', label: 'Commit Id' },
+    { name: 'version', label: 'Version' },
+  ];
 
   constructor(private titlesService: TitlesService, private configurationService: ConfigurationService) { }
 
   ngOnInit() {
     this.titlesService.setHeadAndTopTitle('Help');
     this.configurationService.getHelpInfo()
-    .subscribe(
+      .subscribe(
       (helpInfo) => {
         this.versionsInfo = helpInfo.versionsInfo;
         this.elastestModulesNames = Object.keys(this.versionsInfo);
+        this.initDataTable();
       },
     );
+  }
+
+  initDataTable(): void {
+    if (this.versionsInfo && this.elastestModulesNames) {
+      for (let versionInfo of this.elastestModulesNames) {
+        let obj: any = {
+          imageName: versionInfo,
+          date: this.versionsInfo[versionInfo].date,
+          commitId: this.versionsInfo[versionInfo].commitId,
+          version: this.versionsInfo[versionInfo].name,
+        };
+        this.versionsInfoDataTable.push(obj);
+
+        if (versionInfo.startsWith('elastest/platform')) {
+          this.etCurrentVersion = versionInfo.split(':')[1];
+          if (this.etCurrentVersion === undefined || this.etCurrentVersion === 'latest') {
+            this.etCurrentVersion = 'Latest Stable';
+          }
+        }
+      }
+      this.versionsInfoDataTable = [...this.versionsInfoDataTable];
+    }
   }
 
 }
