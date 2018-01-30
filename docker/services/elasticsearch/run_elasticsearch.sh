2a8fb7856548f492e5257230f3b82278f25ceb35
Fede Diaz <nordri@gmail.com>
2018-01-30 16:28:58 +0100

    reverting changes

--- docker/services/elasticsearch/run_elasticsearch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# fix permissions (wrong if docker mounted volume)
-chown -R elasticsearch:elasticsearch /usr/share/elasticsearch
-
-# now switch to elasticsearch user and run in foreground
-su elasticsearch -s /bin/sh -c "/usr/share/elasticsearch/bin/elasticsearch" 
