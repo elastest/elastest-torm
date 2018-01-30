c49dce273294f98d74cd7854f3731a7cf960d0bd
Fede Diaz <nordri@gmail.com>
2018-01-30 15:30:54 +0100

    simplifying start up script

--- docker/services/elasticsearch/run_elasticsearch.sh
+++ docker/services/elasticsearch/run_elasticsearch.sh
@@ -3,10 +3,5 @@
 # fix permissions (wrong if docker mounted volume)
 chown -R elasticsearch:elasticsearch /usr/share/elasticsearch
 
-CONF_FILE=/usr/share/elasticsearch/config/elasticsearch.yml 
-LOG_DIR=/usr/share/elasticsearch/logs
-CONF_DIR=/usr/share/elasticsearch/config
-
 # now switch to elasticsearch user and run in foreground
-echo Starting: /usr/share/elasticsearch/bin/elasticsearch -Des.default.config=$CONF_FILE -Des.default.path.home=/usr/share/elasticsearch -Des.default.path.logs=$LOG_DIR -Des.default.path.data=/var/lib/elasticsearch -Des.default.path.work=/tmp/elasticsearch -Des.default.path.conf=$CONF_DIR $@
-su elasticsearch -s /bin/sh -c "/usr/share/elasticsearch/bin/elasticsearch -Des.default.config=$CONF_FILE -Des.default.path.home=/usr/share/elasticsearch -Des.default.path.logs=$LOG_DIR -Des.default.path.data=/var/lib/elasticsearch -Des.default.path.work=/tmp/elasticsearch -Des.default.path.conf=$CONF_DIR $@"
+su elasticsearch -s /bin/sh -c "/usr/share/elasticsearch/bin/elasticsearch" 
