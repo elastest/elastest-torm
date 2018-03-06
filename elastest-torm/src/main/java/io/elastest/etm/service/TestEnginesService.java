d559a34ceab6f71fc959d3a2daf20fcde77a8755
edujgurjc <ejg.urjc@gmail.com>
2018-03-06 11:33:52 +0100

    TestEngine localhost bug fixed => get container ip

--- elastest-torm/src/main/java/io/elastest/etm/service/TestEnginesService.java
+++ elastest-torm/src/main/java/io/elastest/etm/service/TestEnginesService.java
@@ -23,206 +23,200 @@ import io.elastest.epm.client.service.DockerComposeService;
 @Service
 public class TestEnginesService {
 
-    private final static Logger log = LoggerFactory.getLogger(TestEnginesService.class);
+	private final static Logger log = LoggerFactory.getLogger(TestEnginesService.class);
 
-    public DockerComposeService dockerComposeService;
-    public DockerService2 dockerService2;
+	public DockerComposeService dockerComposeService;
+	public DockerService2 dockerService2;
 
-    public List<String> enginesList = new ArrayList<>();
+	public List<String> enginesList = new ArrayList<>();
 
-    @Value("${elastest.docker.network}")
-    public String network;
+	@Value("${elastest.docker.network}")
+	public String network;
 
-    @Value("${et.test.engines.path}")
-    public String ET_TEST_ENGINES_PATH;
+	@Value("${et.test.engines.path}")
+	public String ET_TEST_ENGINES_PATH;
 
-    @Value("${exec.mode")
-    public String execmode;
+	@Value("${exec.mode")
+	public String execmode;
 
-    @Value("${et.public.host}")
-    public String etPublicHost;
+	@Value("${et.public.host}")
+	public String etPublicHost;
 
-    @Value("${et.edm.mysql.host}")
-    public String etEdmMysqlHost;
+	@Value("${et.edm.mysql.host}")
+	public String etEdmMysqlHost;
 
-    @Value("${et.edm.mysql.port}")
-    public String etEdmMysqlPort;
+	@Value("${et.edm.mysql.port}")
+	public String etEdmMysqlPort;
 
-    public TestEnginesService(DockerComposeService dockerComposeService,
-            DockerService2 dockerService2) {
-        this.dockerComposeService = dockerComposeService;
-        this.dockerService2 = dockerService2;
-    }
+	public TestEnginesService(DockerComposeService dockerComposeService, DockerService2 dockerService2) {
+		this.dockerComposeService = dockerComposeService;
+		this.dockerService2 = dockerService2;
+	}
 
-    public void registerEngines() {
-        this.enginesList.add("ece");
-        this.enginesList.add("ere"); // It's necessary to auth:
-                                     // https://docs.google.com/document/d/1RMMnJO3rA3KRg-q_LRgpmmvSTpaCPsmfAQjs9obVNeU
-    }
+	public void registerEngines() {
+		this.enginesList.add("ece");
+		this.enginesList.add("ere"); // It's necessary to auth:
+										// https://docs.google.com/document/d/1RMMnJO3rA3KRg-q_LRgpmmvSTpaCPsmfAQjs9obVNeU
+	}
 
-    @PostConstruct
-    public void init() {
-        if (!execmode.equals("normal")) {
-            registerEngines();
-            for (String engine : this.enginesList) {
-                createProject(engine);
-            }
-        }
-    }
+	@PostConstruct
+	public void init() {
+		if (!execmode.equals("normal")) {
+			registerEngines();
+			for (String engine : this.enginesList) {
+				createProject(engine);
+			}
+		}
+	}
 
-    public void createProject(String name) {
-        String dockerComposeYml = getDockerCompose(name);
-        try {
-            dockerComposeService.createProject(name, dockerComposeYml);
-        } catch (Exception e) {
-            log.error("Exception creating project {}", name, e);
-        }
-    }
+	public void createProject(String name) {
+		String dockerComposeYml = getDockerCompose(name);
+		try {
+			dockerComposeService.createProject(name, dockerComposeYml);
+		} catch (Exception e) {
+			log.error("Exception creating project {}", name, e);
+		}
+	}
 
-    public String getDockerCompose(String engineName) {
-        String content = "";
-        try {
-            InputStream inputStream = getClass().getResourceAsStream(
-                    "/" + ET_TEST_ENGINES_PATH + engineName + ".yml");
-            content = IOUtils.toString(inputStream, "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	public String getDockerCompose(String engineName) {
+		String content = "";
+		try {
+			InputStream inputStream = getClass().getResourceAsStream("/" + ET_TEST_ENGINES_PATH + engineName + ".yml");
+			content = IOUtils.toString(inputStream, "UTF-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        return content;
-    }
+		return content;
+	}
 
-    public String createInstance(String engineName) {
-        String url = "";
-        if (!isRunning(engineName)) {
-            try {
-                dockerComposeService.startProject(engineName);
-                insertIntoETNetwork(engineName);
-                url = getServiceUrl(engineName);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return url;
-    }
+	public String createInstance(String engineName) {
+		String url = "";
+		if (!isRunning(engineName)) {
+			try {
+				dockerComposeService.startProject(engineName);
+				insertIntoETNetwork(engineName);
+				url = getServiceUrl(engineName);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return url;
+	}
 
-    public void insertIntoETNetwork(String engineName) {
-        try {
-            for (DockerContainer container : dockerComposeService
-                    .getContainers(engineName).getContainers()) {
-                dockerService2.insertIntoNetwork(network, container.getName());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	public void insertIntoETNetwork(String engineName) {
+		try {
+			for (DockerContainer container : dockerComposeService.getContainers(engineName).getContainers()) {
+				dockerService2.insertIntoNetwork(network, container.getName());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
 
-    public String getServiceUrl(String serviceName) {
-        String url = "";
-        try {
-            for (DockerContainer container : dockerComposeService
-                    .getContainers(serviceName).getContainers()) {
+	public String getServiceUrl(String serviceName) {
+		String url = "";
+		try {
+			for (DockerContainer container : dockerComposeService.getContainers(serviceName).getContainers()) {
 
-                log.debug("Container info: {}", container);
+				log.debug("Container info: {}", container);
 
-                String containerName = serviceName + "_" + serviceName + "_1"; // example:
-                                                                               // ece_ece_1
-                if (container.getName().equals(containerName)) {
-                    String ip = etPublicHost;
+				String containerName = serviceName + "_" + serviceName + "_1"; // example:
+																				// ece_ece_1
+				if (container.getName().equals(containerName)) {
+					String ip = etPublicHost;
 
-                    String port = "";
-                    for (Entry<String, List<PortInfo>> portList : container
-                            .getPorts().entrySet()) {
-                        if (portList.getValue() != null) {
-                            if (ip.equals("localhost")){
-                                port = portList.getKey().split("/")[0];
-                                ip = portList.getValue().get(0).getHostIp();
-                            } else {
-                                port = portList.getValue().get(0).getHostPort();
-                            }
-                            break;
-                            
-                        }
-                    }
+					String port = "";
+					for (Entry<String, List<PortInfo>> portList : container.getPorts().entrySet()) {
+						if (portList.getValue() != null) {
+							if (ip.equals("localhost")) {
+								port = portList.getKey().split("/")[0];
+								ip = dockerService2.getContainerIpByNetwork(containerName, network);
+							} else {
+								port = portList.getValue().get(0).getHostPort();
+							}
+							break;
 
-                    if ("".equals(port)) {
-                        throw new Exception("Port not found");
-                    }
+						}
+					}
 
-                    url = "http://" + ip + ":" + port;
+					if ("".equals(port)) {
+						throw new Exception("Port not found");
+					}
 
-                    if (serviceName.equals("ere")) {
-                        url += "/ere-app";
-                    }
-                    log.debug("Url: " + url);
-                }
-            }
+					url = "http://" + ip + ":" + port;
 
-        } catch (Exception e) {
-            log.error("Service url not exist {}", serviceName, e);
-        }
-        return url;
-    }
+					if (serviceName.equals("ere")) {
+						url += "/ere-app";
+					}
+					log.debug("Url: " + url);
+				}
+			}
 
-    public boolean checkIfEngineUrlIsUp(String engineName) {
-        String url = getServiceUrl(engineName);
-        return checkIfUrlIsUp(url);
-    }
+		} catch (Exception e) {
+			log.error("Service url not exist {}", serviceName, e);
+		}
+		return url;
+	}
 
-    public boolean checkIfUrlIsUp(String engineUrl) {
-        boolean up = false;
-        URL url;
-        try {
-            url = new URL(engineUrl);
-            log.info("Service url to check: " + engineUrl);
-            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
-            int responseCode = huc.getResponseCode();
-            up = (responseCode >= 200 && responseCode <= 299);
-            if (!up) {
-                log.info("Service no ready at url: " + engineUrl);
-                return up;
-            }
-        } catch (IOException e) {
-            return false;
-        }
+	public boolean checkIfEngineUrlIsUp(String engineName) {
+		String url = getServiceUrl(engineName);
+		return checkIfUrlIsUp(url);
+	}
 
-        log.info("Service ready at url: " + engineUrl);
+	public boolean checkIfUrlIsUp(String engineUrl) {
+		boolean up = false;
+		URL url;
+		try {
+			url = new URL(engineUrl);
+			log.info("Service url to check: " + engineUrl);
+			HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+			int responseCode = huc.getResponseCode();
+			up = (responseCode >= 200 && responseCode <= 299);
+			if (!up) {
+				log.info("Service no ready at url: " + engineUrl);
+				return up;
+			}
+		} catch (IOException e) {
+			return false;
+		}
 
-        return up;
-    }
+		log.info("Service ready at url: " + engineUrl);
 
-    public void stopInstance(String engineName) {
-        try {
-            dockerComposeService.stopProject(engineName);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		return up;
+	}
 
-    public String getUrlIfIsRunning(String engineName) {
-        return getServiceUrl(engineName);
-    }
+	public void stopInstance(String engineName) {
+		try {
+			dockerComposeService.stopProject(engineName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public Boolean isRunning(String engineName) {
-        try {
-            for (DockerContainer container : dockerComposeService
-                    .getContainers(engineName).getContainers()) {
-                String containerName = engineName + "_" + engineName + "_1";
-                if (container.getName().equals(containerName)) {
-                    return container.isRunning();
-                }
-            }
-            return false;
+	public String getUrlIfIsRunning(String engineName) {
+		return getServiceUrl(engineName);
+	}
 
-        } catch (Exception e) {
-            log.error("Engine not started or not exist {}", engineName, e);
-            return false;
-        }
-    }
+	public Boolean isRunning(String engineName) {
+		try {
+			for (DockerContainer container : dockerComposeService.getContainers(engineName).getContainers()) {
+				String containerName = engineName + "_" + engineName + "_1";
+				if (container.getName().equals(containerName)) {
+					return container.isRunning();
+				}
+			}
+			return false;
 
-    public List<String> getTestEngines() {
-        return enginesList;
-    }
+		} catch (Exception e) {
+			log.error("Engine not started or not exist {}", engineName, e);
+			return false;
+		}
+	}
+
+	public List<String> getTestEngines() {
+		return enginesList;
+	}
 
 }
