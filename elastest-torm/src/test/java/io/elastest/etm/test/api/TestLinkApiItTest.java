e641865d7bd192698a026d1ef83c893f47824aab
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-04-27 09:26:20 +0200

    TL Api It Test fixed and enabled

--- elastest-torm/src/test/java/io/elastest/etm/test/api/TestLinkApiItTest.java
+++ elastest-torm/src/test/java/io/elastest/etm/test/api/TestLinkApiItTest.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +18,10 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -44,7 +47,7 @@ public class TestLinkApiItTest extends EtmApiItTest {
     protected String tlApiPath = "/api/testlink";
 
     @Test
-    @Disabled
+    // @Disabled
     @DisplayName("Create TestLink Data and Test In ElasTest (Integration Test)")
     void createSampleTLDataTest() throws InterruptedException, IOException {
         log.info("Creating Sample Data in TestLink...");
@@ -92,10 +95,11 @@ public class TestLinkApiItTest extends EtmApiItTest {
             testCase.setAuthorLogin("admin");
             testCase.setSummary("This is a Summary");
             testCase.setPreconditions("This is a precondition");
+            testCase.setKeywords(new ArrayList<>());
 
             testCase = this.createTlTestCase(testCase);
             testCase.setTestProjectId(project.getId());
-            log.info("Associating TL Case '{}' to Test Plan", caseName);
+            log.info("Associating TL Case '{}' to Test Plan", testCase);
             this.addTLTestCaseToTestPlan(testCase, plan.getId());
             cases.add(testCase);
         }
@@ -136,9 +140,8 @@ public class TestLinkApiItTest extends EtmApiItTest {
         if (this.tlTestProjectExists(project.getName())) {
             return this.getTLTestProject(project.getName());
         } else {
-            String jsonPj = this.objectToJson(project);
-            return this.httpClient.postForEntity(tlApiPath + "/project", jsonPj,
-                    TestProject.class).getBody();
+            return this.httpClient.postForEntity(tlApiPath + "/project",
+                    project, TestProject.class).getBody();
         }
     }
 
@@ -163,9 +166,8 @@ public class TestLinkApiItTest extends EtmApiItTest {
         if (this.tlTestSuiteExists(suite.getName())) {
             return this.getTLTestSuite(suite.getName());
         } else {
-            String jsonSuite = this.objectToJson(suite);
             return this.httpClient.postForEntity(tlApiPath + "/project/"
-                    + suite.getTestProjectId() + "/suite", jsonSuite,
+                    + suite.getTestProjectId() + "/suite", suite,
                     TestSuite.class).getBody();
         }
     }
@@ -203,23 +205,24 @@ public class TestLinkApiItTest extends EtmApiItTest {
         if (this.tlTestCaseExists(testCase)) {
             return this.getTLTestCase(testCase);
         } else {
-            String jsonCase = this.objectToJson(testCase);
-            String jsonSavedCase = this.httpClient
-                    .postForEntity(
-                            tlApiPath + "/project/suite/"
-                                    + testCase.getTestSuiteId() + "/case",
-                            jsonCase, String.class)
-                    .getBody();
-            return this.getTestCaseFromJson(jsonSavedCase);
+            HttpEntity<String> jsonCaseRequest = this
+                    .getTestCaseRequest(testCase);
+            ResponseEntity<String> response = this.httpClient.exchange(
+                    tlApiPath + "/project/suite/" + testCase.getTestSuiteId()
+                            + "/case",
+                    HttpMethod.POST, jsonCaseRequest, String.class);
+            return this.getTestCaseFromJson(response.getBody());
         }
     }
 
-    protected void addTLTestCaseToTestPlan(TestCase testCase, Integer planId)
+    protected Integer addTLTestCaseToTestPlan(TestCase testCase, Integer planId)
             throws JsonParseException, JsonMappingException, IOException {
-        String jsonCase = this.objectToJson(testCase);
-        this.httpClient.postForEntity(
-                tlApiPath + "/project/plan/" + planId + "/case/add", jsonCase,
-                Object.class);
+        HttpEntity<String> jsonCaseRequest = this.getTestCaseRequest(testCase);
+
+        ResponseEntity<String> responseEntity = this.httpClient.exchange(
+                tlApiPath + "/project/plan/" + planId + "/case/add",
+                HttpMethod.POST, jsonCaseRequest, String.class);
+        return this.getObjectFromJson(responseEntity.getBody(), Integer.class);
     }
 
     /* **************** */
@@ -243,10 +246,8 @@ public class TestLinkApiItTest extends EtmApiItTest {
         if (this.tlTestPlanExists(testPlan.getName())) {
             return this.getTLTestPlan(testPlan.getName());
         } else {
-            String jsonPlan = this.objectToJson(testPlan);
-
             return this.httpClient.postForEntity(tlApiPath + "/project/plan",
-                    jsonPlan, TestPlan.class).getBody();
+                    testPlan, TestPlan.class).getBody();
         }
     }
 
@@ -270,9 +271,8 @@ public class TestLinkApiItTest extends EtmApiItTest {
         if (this.tlBuildExists(build.getName())) {
             return this.getTLBuild(build.getName());
         } else {
-            String jsonBuild = this.objectToJson(build);
             return this.httpClient
-                    .postForEntity(tlApiPath + "/project/plan/build", jsonBuild,
+                    .postForEntity(tlApiPath + "/project/plan/build", build,
                             Build.class)
                     .getBody();
         }
@@ -290,6 +290,17 @@ public class TestLinkApiItTest extends EtmApiItTest {
             return mapper.readValue(json, clazz);
         }
         return null;
+    }
+
+    protected HttpEntity<String> getTestCaseRequest(TestCase testCase)
+            throws JsonProcessingException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Accept", "*/*");
+
+        String jsonCase = this.objectToJson(testCase);
+
+        return new HttpEntity<String>(jsonCase, headers);
     }
 
     protected TestCase getTestCaseFromJson(String json)
