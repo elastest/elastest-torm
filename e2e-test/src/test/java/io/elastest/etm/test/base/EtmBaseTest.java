8065152c4efc9e242bf452c7a9993c5189b7421b
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-06-01 10:38:41 +0200

    E2E => fix for secure ET url

--- e2e-test/src/test/java/io/elastest/etm/test/base/EtmBaseTest.java
+++ e2e-test/src/test/java/io/elastest/etm/test/base/EtmBaseTest.java
@@ -102,7 +102,7 @@ public class EtmBaseTest {
 
         if (secureElastest) {
             String split_url[] = tormUrl.split("//");
-            secureTorm = "https://" + eUser + ":" + ePassword + "@"
+            secureTorm = split_url[0] + "//" + eUser + ":" + ePassword + "@"
                     + split_url[1];
 
             this.tormApiUrl = secureTorm + (secureTorm.endsWith("/")
