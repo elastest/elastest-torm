a24a49a6423dd294f28dd11dc07a4b948964258a
franciscoRdiaz <francisco.diazbayon@gmail.com>
2018-06-27 10:38:03 +0200

    Try to fix the issue reading files

--- elastest-torm/src/main/java/io/elastest/etm/utils/FilesService.java
+++ elastest-torm/src/main/java/io/elastest/etm/utils/FilesService.java
@@ -85,7 +85,7 @@ public class FilesService {
         try {
             logger.info("Epm files path {}", path);
             logger.info("Package name {}", fileName);
-            file = ResourceUtils.getFile(path);
+            file = new File(getClass().getClassLoader().getResource(path + "/" + fileName).getFile());
             if (file.exists()) {
                 logger.info("Load file in prod mode");
                 logger.info("File name: {}", fileName);
