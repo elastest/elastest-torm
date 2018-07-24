cea8e8ee68230b6362f07ca319e55f26c24e4e8e
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-07-24 11:50:00 +0200

    Jenkinsfile => bind tcp/beats/http ports

--- scripts/it.sh
+++ scripts/it.sh
@@ -53,14 +53,19 @@ LOGSTASH_IP=$(containerIp "etm-logstash")
 ET_ESM_HOST=$(containerIp "esm")
 EXEC_MODE='normal'
 ET_ESM_SS_DESC_FILES_PATH='esm_services/'
-ET_ETM_INCONTAINER=true
+ET_ETM_INCONTAINER=false
 ELASTEST_DOCKER_NETWORK=${projectName}_elastest
 ET_IN_PROD=true
 ET_PROXY_PORT=37000
 ET_ETM_LOGSTASH_CONTAINER_NAME=${projectName}_etm-logstash_1
 # TestLink Container Name
 ET_ETM_TESTLINK_HOST=$(docker ps | awk '{print $NF}' | grep ".*etm-testlink.*")
-ET_ETM_CONTAINER_NAME=$HOSTNAME
+
+ET_ETM_LSTCP_PORT=37500
+ET_ETM_LSHTTP_PORT=37501
+ET_ETM_LSBEATS_PORT=37502
+
+
 
 
 # Execute Integration tests
@@ -71,7 +76,7 @@ cd ../elastest-torm
 
 docker ps -a
 
-mvn -Det.edm.mysql.host=${ET_EDM_MYSQL_HOST} -Det.edm.mysql.port=${ET_EDM_MYSQL_PORT} -Det.etm.rabbit.host=${ET_ETM_RABBIT_HOST} -Det.etm.rabbit.port=${ET_ETM_RABBIT_PORT} -Det.edm.elasticsearch.api=http://${ELASTICSEARCH_IP}:9200/ -Dlogstash.host=${LOGSTASH_IP} -Det.esm.api=http://${ET_ESM_HOST}:37005/ -Delastest.execution.mode=${EXEC_MODE} -Det.esm.ss.desc.files.path=${ET_ESM_SS_DESC_FILES_PATH} -Det.etm.incontainer=${ET_ETM_INCONTAINER} -Delastest.docker.network=${ELASTEST_DOCKER_NETWORK} -Det.in.prod=${ET_IN_PROD} -Det.proxy.port=${ET_PROXY_PORT} -Det.etm.logstash.container.name=${ET_ETM_LOGSTASH_CONTAINER_NAME} -Det.etm.testlink.host=${ET_ETM_TESTLINK_HOST} -Det.etm.container.name=${ET_ETM_CONTAINER_NAME} package
+mvn -Det.edm.mysql.host=${ET_EDM_MYSQL_HOST} -Det.edm.mysql.port=${ET_EDM_MYSQL_PORT} -Det.etm.rabbit.host=${ET_ETM_RABBIT_HOST} -Det.etm.rabbit.port=${ET_ETM_RABBIT_PORT} -Det.edm.elasticsearch.api=http://${ELASTICSEARCH_IP}:9200/ -Dlogstash.host=${LOGSTASH_IP} -Det.esm.api=http://${ET_ESM_HOST}:37005/ -Delastest.execution.mode=${EXEC_MODE} -Det.esm.ss.desc.files.path=${ET_ESM_SS_DESC_FILES_PATH} -Det.etm.incontainer=${ET_ETM_INCONTAINER} -Delastest.docker.network=${ELASTEST_DOCKER_NETWORK} -Det.in.prod=${ET_IN_PROD} -Det.proxy.port=${ET_PROXY_PORT} -Det.etm.logstash.container.name=${ET_ETM_LOGSTASH_CONTAINER_NAME} -Det.etm.testlink.host=${ET_ETM_TESTLINK_HOST} -Det.etm.lstcp.port=${ET_ETM_LSTCP_PORT} -Det.etm.lshttp.port=${ET_ETM_LSHTTP_PORT} -Det.etm.lsbeats.port=${ET_ETM_LSBEATS_PORT} package
 
 mvnExit=$?
 
