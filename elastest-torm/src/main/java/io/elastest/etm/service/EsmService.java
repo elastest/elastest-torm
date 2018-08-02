4d725b205ead9a7a09b0ba2315c4fd20643d5c5c
franciscoRdiaz <francisco.diazbayon@gmail.com>
2018-08-02 15:58:42 +0200

    Fix: Set manifest_type to epm when ElasTest is running in MS mode

--- elastest-torm/src/main/java/io/elastest/etm/service/EsmService.java
+++ elastest-torm/src/main/java/io/elastest/etm/service/EsmService.java
@@ -371,7 +371,7 @@ public class EsmService {
                 + serviceDefJson
                         .get("manifest").get("manifest_content").toString()
                 + ", \"manifest_type\": "
-                + (EpmService.etMasterSlaveMode ? "epm"
+                + (EpmService.etMasterSlaveMode ? "\"epm\""
                         : serviceDefJson.get("manifest").get("manifest_type")
                                 .toString())
                 + ", \"plan_id\": "
