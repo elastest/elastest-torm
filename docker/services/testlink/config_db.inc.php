c2a98ea6ba09484bee254d12b418dbec94a56821
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-09-07 13:14:37 +0200

    Return TL to rc8 version

---
+++ docker/services/testlink/config_db.inc.php
@@ -0,0 +1,9 @@
+<?php
+# Replace original file to this for ElasTest
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'DATABASE_USER');
+define('DB_PASS', 'DATABASE_PASS');
+define('DB_HOST', 'DATABASE_HOST:DATABASE_PORT');
+define('DB_NAME', 'bitnami_testlink');
+?>
+
