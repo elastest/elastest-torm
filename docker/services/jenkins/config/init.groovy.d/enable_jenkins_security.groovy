cbac717b73a4ff34bfac5de45141565c650d7897
franciscoRdiaz <francisco.diazbayon@gmail.com>
2018-09-19 08:36:22 +0200

    Jenkins: Set credentials based on env vars

--- docker/services/jenkins/config/init.groovy.d/enable_jenkins_security.groovy
+++ docker/services/jenkins/config/init.groovy.d/enable_jenkins_security.groovy
@@ -5,11 +5,23 @@ import hudson.security.*
 import hudson.model.RestartListener
 
 def instance = Jenkins.getInstance()
+def env = System.getenv()
 
 println "--> Creating local user 'elastest'"
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount('elastest','elastest')
+def user = null
+def pass = null
+
+if ((env['ET_USER'] && env['ET_USER'] != "none") 
+    && (env['ET_PASS'] && env['ET_PASS'] != "none" )) {
+    user = env['ET_USER']
+    pass = env['ET_PASS']
+} else {
+    user = 'elastest'
+    pass = 'elastest'
+}
+hudsonRealm.createAccount(user,pass)
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
