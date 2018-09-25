8e4e4b1db3ff80d65a2dfc2f4ae70f5fa082a88c
franciscoRdiaz <francisco.diazbayon@gmail.com>
2018-09-25 13:15:40 +0200

    Fix issue serving static content

--- elastest-torm/src/main/java/io/elastest/etm/config/MvcConfig.java
+++ elastest-torm/src/main/java/io/elastest/etm/config/MvcConfig.java
@@ -16,30 +16,19 @@
  */
 package io.elastest.etm.config;
 
-import java.util.List;
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.Validator;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    final Logger logger = getLogger(lookup().lookupClass());
 
     @Value("${registry.contextPath}")
     private String registryContextPath;
@@ -50,116 +39,12 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler(registryContextPath + "/**")
-                .addResourceLocations("file:" + etSharedFolder);
+                .addResourceLocations("file:" + etSharedFolder + "/");
+        logger.debug("Data folder: {}", etSharedFolder);
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**").allowedMethods("*").allowedOrigins("*");
     }
-
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void configureContentNegotiation(
-            ContentNegotiationConfigurer configurer) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void configureDefaultServletHandling(
-            DefaultServletHandlerConfigurer configurer) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void addArgumentResolvers(
-            List<HandlerMethodArgumentResolver> argumentResolvers) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void addReturnValueHandlers(
-            List<HandlerMethodReturnValueHandler> returnValueHandlers) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void configureMessageConverters(
-            List<HttpMessageConverter<?>> converters) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void extendMessageConverters(
-            List<HttpMessageConverter<?>> converters) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void configureHandlerExceptionResolvers(
-            List<HandlerExceptionResolver> exceptionResolvers) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void extendHandlerExceptionResolvers(
-            List<HandlerExceptionResolver> exceptionResolvers) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public Validator getValidator() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public MessageCodesResolver getMessageCodesResolver() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
 }
