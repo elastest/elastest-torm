2585f37079e5ca43a8db5f135074507d609665c5
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-10-15 17:13:27 +0200

    GUI multi => show summary

--- elastest-torm-gui/src/app/elastest-etm/tjob-exec/parent-tjob-exec-report-view/parent-tjob-exec-report-view.component.ts
+++ elastest-torm-gui/src/app/elastest-etm/tjob-exec/parent-tjob-exec-report-view/parent-tjob-exec-report-view.component.ts
@@ -13,16 +13,29 @@ export class ParentTjobExecReportViewComponent implements OnInit {
   @Input()
   model: TJobExecModel;
 
+  // test * config
   uniqueConfigRows: any[] = [];
+  // config1 * config2
   twoConfigRows: any[] = [];
+  // test * config1|config2
+  config1Config2Rows: any[] = [];
+  // test * config2|config1
+  config2Config1Rows: any[] = [];
 
   uniqueConfigColumns: any[] = [{ name: 'caseName', label: 'Case Name' }];
   twoConfigColumns: any[] = [];
 
+  config1Config2Columns: any[] = [{ name: 'caseName', label: 'Case Name' }];
+  config2Config1Columns: any[] = [{ name: 'caseName', label: 'Case Name' }];
+
   constructor() {}
 
   ngOnInit() {
-    this.loadData();
+    try {
+      this.loadData();
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   loadData(): void {
@@ -39,6 +52,8 @@ export class ParentTjobExecReportViewComponent implements OnInit {
         this.loadUniqueConfigRows();
       } else if (this.model.tJob.multiConfigurations.length === 2) {
         this.loadTwoConfigRows();
+        this.loadConfig1Config2Rows();
+        this.loadConfig2Config1Rows();
       }
     }
   }
@@ -114,6 +129,38 @@ export class ParentTjobExecReportViewComponent implements OnInit {
       this.twoConfigRows = [...this.twoConfigRows];
     }
   }
+
+  loadConfig1Config2Rows(): void {
+    let config1Name: string = this.model.tJob.multiConfigurations[0].name;
+    let config2Name: string = this.model.tJob.multiConfigurations[1].name;
+
+    let suitePos: number = 0;
+    let casePos: number = 0;
+    for (let testSuite of this.model.execChilds[0].testSuites) {
+      for (let testCase of testSuite.testCases) {
+        let row: any = {
+          caseName: testSuite.name + ' / ' + testCase.name,
+        };
+        for (let child of this.model.execChilds) {
+          let param1: ParameterModel = this.getParameterByName(config1Name, child.parameters);
+          let param2: ParameterModel = this.getParameterByName(config2Name, child.parameters);
+
+          let colName: string = param1.value + ' | ' + param2.value;
+          let colValue: string = child.testSuites[suitePos].testCases[casePos].getResult();
+          row[colName] = colValue;
+          if (casePos === 0) {
+            // Add column only first
+            this.config1Config2Columns.push({ name: colName, label: colName });
+          }
+        }
+        this.config1Config2Rows.push(row);
+        casePos++;
+      }
+      suitePos++;
+    }
+  }
+
+  loadConfig2Config1Rows(): void {}
 
   getParameterByName(name: string, params: ParameterModel[]): ParameterModel {
     for (let param of params) {
