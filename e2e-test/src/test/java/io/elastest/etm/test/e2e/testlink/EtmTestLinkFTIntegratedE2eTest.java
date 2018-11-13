cab954dabfd24eeb39bc2cba7b1312b11d7dd773
franciscoRdiaz <francisco.diazbayon@gmail.com>
2018-11-13 11:39:46 +0100

    et-in-et: rename test

--- e2e-test/src/test/java/io/elastest/etm/test/e2e/testlink/EtmTestLinkFTIntegratedE2eTest.java
+++ e2e-test/src/test/java/io/elastest/etm/test/e2e/testlink/EtmTestLinkFTIntegratedE2eTest.java
@@ -58,7 +58,7 @@ import io.github.bonigarcia.SeleniumExtension;
 @DisplayName("ETM E2E test of TestLink")
 @ExtendWith(SeleniumExtension.class)
 @TestInstance(Lifecycle.PER_CLASS)
-public class EtmTestLinkFTIntegratedE2eTest2 extends EtmTestLinkBaseTest {
+public class EtmTestLinkFTIntegratedE2eTest extends EtmTestLinkBaseTest {
 
     static String fullteachingUrl = null;
 
