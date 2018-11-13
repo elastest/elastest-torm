54fb202fc61cfb09f23adbac03a9a91fe40fffb9
franciscoRdiaz <francisco.diazbayon@gmail.com>
2018-11-13 17:47:32 +0100

    et-in-et: add new test for the ElasTest plugin

---
+++ e2e-test/src/test/java/io/elastest/etm/test/e2e/plugin/EJWhitInstallEtInEtTest.java
@@ -0,0 +1,87 @@
+package io.elastest.etm.test.e2e.plugin;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+
+import io.elastest.etm.test.base.EtmPluginBaseTest;
+import io.github.bonigarcia.BrowserType;
+
+public class EJWhitInstallEtInEtTest extends EtmPluginBaseTest {
+    final Logger log = getLogger(lookup().lookupClass());
+
+    final String unitTestScript = "node{\n" + "    elastest(tss: ['EUS']) {\n"
+            + "        stage ('Executing Test') {\n"
+            + "            echo 'Print env variables'\n"
+            + "            sh 'env'\n" + "            mvnHome = tool 'M3.3.9'\n"
+            + "            echo 'Cloning repository'\n"
+            + "            git 'https://github.com/elastest/demo-projects'\n"
+            + "            echo 'Run test'\n"
+            + "            sh \"cd ./unit-java-test/;'${mvnHome}/bin/mvn' -DforkCount=0 test\"\n";// +
+    
+    final String jobName = "PJob_1";
+    
+    @Test
+    @DisplayName("Pipeline plugin")
+    void testPipelineJob(ChromeDriver cDriver) throws Exception {
+        RemoteWebDriver driver = null;
+        if (this.eusURL != null) {
+            driver = setupTestBrowser(new Object() {
+            }.getClass().getEnclosingMethod().getName(), BrowserType.CHROME,
+                    driver);
+        } else {
+            driver = cDriver;
+            this.driver = cDriver;
+        }
+        navigateTo(driver, jenkinsPluginManagerAd);
+        loginOnJenkins(driver);
+        installElasTestPlugin(driver);
+        navigateTo(driver, pluginSettings);
+        pluginConfiguration(driver);
+        navigateTo(driver, jenkinsCIUrl);
+
+        // Creation of a new Pipeline Job
+        driver.findElement(By.linkText("New Item")).click();
+        createPipelineJob(driver, "PJob_1", unitTestScript);
+
+        executeJob(driver);
+
+        String linkElasTest = driver
+                .findElement(By.linkText("Open in ElasTest"))
+                .getAttribute("href");
+        if (secureElastest) {
+            String split_url[] = linkElasTest.split("//");
+            linkElasTest = split_url[0] + "//" + eUser + ":" + ePassword + "@"
+                    + split_url[1];
+            navigateTo(driver, linkElasTest);
+        } else {
+            driver.findElement(By.linkText("Open in ElasTest")).click();
+        }
+
+        WebDriverWait waitLogs = new WebDriverWait(driver, 60);
+        log.info("Wait for build sucess traces");
+        checkFinishTJobExec(driver, 180, "SUCCESS", false);
+        WebElement logsView = driver.findElement(By.xpath(
+                "//logs-view"));
+        JavascriptExecutor jse2 = (JavascriptExecutor) driver;
+        try {
+            jse2.executeScript("arguments[0].scrollIntoView()", logsView);
+            waitLogs.until(textToBePresentInElementLocated(
+                    By.tagName("logs-view"), "BUILD SUCCESS"));
+        } catch (Exception te) {
+            jse2.executeScript("arguments[0].scrollIntoView()", logsView);
+            waitLogs.until(textToBePresentInElementLocated(
+                    By.tagName("logs-view"), "BUILD SUCCESS"));
+        }
+    }
+}
