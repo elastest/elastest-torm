6bc6407c3c4698a855beb90f9901443441345760
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-02-08 08:53:48 +0100

    Re-Add lost TCP server at 5001 in MINI for EMS beats traces

--- elastest-torm/src/main/java/io/elastest/etm/service/TcpServerService.java
+++ elastest-torm/src/main/java/io/elastest/etm/service/TcpServerService.java
@@ -31,12 +31,140 @@ public class TcpServerService {
     @Value("${et.etm.lstcp.port}")
     public String lsTcpPort;
 
+    // For EMS TCP beat metrics
+    @Value("${et.etm.internal.lstcp.port}")
+    public String internalLsTcpPort;
+
     private Map<String, Long> cachedTimeDifference = new HashMap<>();
 
-    private TCPNetSyslogRFC5424Server server;
+    private TCPNetSyslogRFC5424Server tcpServer;
     private int tcpPort;
+
+    private TCPNetSyslogRFC5424Server tcpServerForEMSBeats;
+    private int tcpPortForEMSBeats;
+
     private TracesService tracesService;
     public UtilsService utilsService;
+
+    SyslogRFC5424ServerSessionEventHandlerIF tcpLogsHandler = new SyslogRFC5424ServerSessionEventHandlerIF() {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void event(Object session, SyslogServerIF syslogServer,
+                SocketAddress socketAddress, SyslogRFC5424ServerEvent event) {
+            // Process
+            try {
+                String finalLogMsg = event.getMsgid() + " - "
+                        + event.getMessage();
+
+                try {
+                    Date timestamp = event.getDate();
+
+                    // Cache time difference by hostname
+                    String key = event.getHostName();
+                    // If is empty or null, use containerName
+                    if (key == null || "".equals(key)) {
+                        String containerName = tracesService
+                                .getContainerNameFromMessage(finalLogMsg);
+                        key = containerName;
+                    }
+
+                    logger.trace("Received Date {}", timestamp);
+                    timestamp = processDateIfIsNecessary(timestamp, finalLogMsg,
+                            key);
+                    logger.trace("Modified Date {}", timestamp);
+
+                    // Process trace
+                    tracesService.processTcpTrace(finalLogMsg, timestamp);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    logger.error("Error on parse timestamp {}:",
+                            event.getDate(), e);
+                }
+            } catch (Exception e) {
+                // RAW: empty raw;
+            }
+
+        }
+
+        @Override
+        public Object sessionOpened(SyslogServerIF syslogServer,
+                SocketAddress socketAddress) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public void exception(Object session, SyslogServerIF syslogServer,
+                SocketAddress socketAddress, Exception exception) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void sessionClosed(Object session, SyslogServerIF syslogServer,
+                SocketAddress socketAddress, boolean timeout) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void initialize(SyslogServerIF syslogServer) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void destroy(SyslogServerIF syslogServer) {
+            // TODO Auto-generated method stub
+
+        }
+
+    };
+
+    SyslogRFC5424ServerSessionEventHandlerIF tcpEMSBeatsHandler = new SyslogRFC5424ServerSessionEventHandlerIF() {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void event(Object session, SyslogServerIF syslogServer,
+                SocketAddress socketAddress, SyslogRFC5424ServerEvent event) {
+            logger.info("EMS tcp beats trace: {}", event.getRaw());
+
+        }
+
+        @Override
+        public Object sessionOpened(SyslogServerIF syslogServer,
+                SocketAddress socketAddress) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public void exception(Object session, SyslogServerIF syslogServer,
+                SocketAddress socketAddress, Exception exception) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void sessionClosed(Object session, SyslogServerIF syslogServer,
+                SocketAddress socketAddress, boolean timeout) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void initialize(SyslogServerIF syslogServer) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void destroy(SyslogServerIF syslogServer) {
+            // TODO Auto-generated method stub
+
+        }
+
+    };
 
     public TcpServerService(TracesService tracesService,
             UtilsService utilsService) {
@@ -47,94 +175,63 @@ public class TcpServerService {
     @PostConstruct
     void init() throws InterruptedException {
         if (utilsService.isElastestMini()) {
+            /* *** Normal TCP SERVER *** */
             tcpPort = Integer.parseInt(lsTcpPort);
-            TCPNetSyslogRFC5424ServerConfig serverConfig = new TCPNetSyslogRFC5424ServerConfig(
+            TCPNetSyslogRFC5424ServerConfig tcpServerConfig = new TCPNetSyslogRFC5424ServerConfig(
                     tcpPort);
 
-            SyslogRFC5424ServerSessionEventHandlerIF handler = new SyslogRFC5424ServerSessionEventHandlerIF() {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public void event(Object session, SyslogServerIF syslogServer,
-                        SocketAddress socketAddress,
-                        SyslogRFC5424ServerEvent event) {
-                    // Process
-                    try {
-                        String finalLogMsg = event.getMsgid() + " - "
-                                + event.getMessage();
-
-                        try {
-                            Date timestamp = event.getDate();
-
-                            // Cache time difference by hostname
-                            String key = event.getHostName();
-                            // If is empty or null, use containerName
-                            if (key == null || "".equals(key)) {
-                                String containerName = tracesService
-                                        .getContainerNameFromMessage(
-                                                finalLogMsg);
-                                key = containerName;
-                            }
-
-                            logger.trace("Received Date {}", timestamp);
-                            timestamp = processDateIfIsNecessary(timestamp,
-                                    finalLogMsg, key);
-                            logger.trace("Modified Date {}", timestamp);
-
-                            // Process trace
-                            tracesService.processTcpTrace(finalLogMsg,
-                                    timestamp);
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            logger.error("Error on parse timestamp {}:",
-                                    event.getDate(), e);
-                        }
-                    } catch (Exception e) {
-                        // RAW: empty raw;
-                    }
-
-                }
-
-                @Override
-                public Object sessionOpened(SyslogServerIF syslogServer,
-                        SocketAddress socketAddress) {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public void exception(Object session,
-                        SyslogServerIF syslogServer,
-                        SocketAddress socketAddress, Exception exception) {
-                    // TODO Auto-generated method stub
-
-                }
-
-                @Override
-                public void sessionClosed(Object session,
-                        SyslogServerIF syslogServer,
-                        SocketAddress socketAddress, boolean timeout) {
-                    // TODO Auto-generated method stub
-
-                }
-
-                @Override
-                public void initialize(SyslogServerIF syslogServer) {
-                    // TODO Auto-generated method stub
-
-                }
-
-                @Override
-                public void destroy(SyslogServerIF syslogServer) {
-                    // TODO Auto-generated method stub
-
-                }
-
-            };
-
-            serverConfig.addEventHandler(handler);
-            server = (TCPNetSyslogRFC5424Server) SyslogServer
-                    .createThreadedInstance("tcp_session", serverConfig);
+            tcpServerConfig.addEventHandler(tcpLogsHandler);
+            tcpServer = (TCPNetSyslogRFC5424Server) SyslogServer
+                    .createThreadedInstance("tcp_session", tcpServerConfig);
             logger.info("Listen at {} TCP Port", tcpPort);
+
+            /* *** EMS TCP SERVER*** */
+
+            tcpPortForEMSBeats = Integer.parseInt(internalLsTcpPort);
+            TCPNetSyslogRFC5424ServerConfig emsTcpServerConfig = new TCPNetSyslogRFC5424ServerConfig(
+                    tcpPortForEMSBeats);
+
+            emsTcpServerConfig.addEventHandler(tcpEMSBeatsHandler);
+            tcpServerForEMSBeats = (TCPNetSyslogRFC5424Server) SyslogServer
+                    .createThreadedInstance("ems_tcp_session",
+                            emsTcpServerConfig);
+            logger.info("Listen at {} TCP Port for EMS Beats",
+                    tcpPortForEMSBeats);
+
+        }
+    }
+
+    @PreDestroy
+    void stopServer() throws InterruptedException {
+        if (utilsService.isElastestMini()) {
+            /* *** Normal TCP SERVER *** */
+            logger.info("Starting shuting down TCP server");
+            if (tcpServer.getThread().isAlive()) {
+                try {
+                    logger.info("Interrupting TCP server thread");
+                    tcpServer.getThread().interrupt();
+                } catch (Exception e) {
+                    logger.error("Error on Interrupting TCP server thread");
+                }
+            }
+            logger.info("Shuting down TCP server");
+            tcpServer.shutdown();
+            SyslogServer.shutdown();
+
+            /* *** EMS TCP SERVER*** */
+            logger.info("Starting shuting down EMS Beats TCP server");
+            if (tcpServerForEMSBeats.getThread().isAlive()) {
+                try {
+                    logger.info("Interrupting EMS Beats TCP server thread");
+                    tcpServerForEMSBeats.getThread().interrupt();
+                } catch (Exception e) {
+                    logger.error(
+                            "Error on Interrupting EMS Beats TCP server thread");
+                }
+            }
+            logger.info("Shuting down EMS Beats TCP server");
+            tcpServerForEMSBeats.shutdown();
+            SyslogServer.shutdown();
         }
     }
 
@@ -173,23 +270,4 @@ public class TcpServerService {
         }
 
     }
-
-    @PreDestroy
-    void stopServer() throws InterruptedException {
-        if (utilsService.isElastestMini()) {
-            logger.info("Starting shuting down TCP server");
-            if (server.getThread().isAlive()) {
-                try {
-                    logger.info("Interrupting TCP server thread");
-                    server.getThread().interrupt();
-                } catch (Exception e) {
-                    logger.error("Error on Interrupting TCP server thread");
-                }
-            }
-            logger.info("Shuting down TCP server");
-            server.shutdown();
-            SyslogServer.shutdown();
-        }
-    }
-
 }
