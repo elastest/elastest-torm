53e2579cf5da6789e5ee28768beb0ef537ded4d7
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-05-09 10:37:54 +0200

    Fix filebeat definitively

--- docker/services/filebeat/stop_k8s_filebeat.sh
+++ docker/services/filebeat/stop_k8s_filebeat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 kubectl --namespace=kube-system delete ds/filebeat --cascade=true
-kubectl -n kube-system delete cm/filebeat-inputs
+#kubectl -n kube-system delete cm/filebeat-inputs
 kubectl -n kube-system delete cm/filebeat-config
