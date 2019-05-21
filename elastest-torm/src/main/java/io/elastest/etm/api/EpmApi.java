93bcd35aa8837da8f5aac8796f3f1f03dca5d225
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-05-21 14:34:33 +0200

    Epm api implemented

--- elastest-torm/src/main/java/io/elastest/etm/api/EpmApi.java
+++ elastest-torm/src/main/java/io/elastest/etm/api/EpmApi.java
@@ -1,13 +1,19 @@
 package io.elastest.etm.api;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.elastest.epm.client.model.Cluster;
+import io.elastest.epm.client.model.Worker;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
@@ -41,5 +47,97 @@ public interface EpmApi extends EtmApiRoot {
                     "application/json" }, method = RequestMethod.POST)
     ResponseEntity<Boolean> uploadNodeTarFile(
             @RequestParam(value = "file") MultipartFile file);
+
+    /* ******************************************* */
+    /* ***************** Cluster ***************** */
+    /* ******************************************* */
+
+    @ApiOperation(value = "Creates a new Cluster", notes = "Creates a new cluster.", response = Cluster.class, tags = {
+            "EPM", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Creation successful", response = Cluster.class),
+            @ApiResponse(code = 405, message = "Invalid input") })
+    @RequestMapping(value = "/epm/cluster", produces = {
+            "application/json" }, consumes = {
+                    "application/json" }, method = RequestMethod.POST)
+    ResponseEntity<Cluster> createCluster();
+
+    @ApiOperation(value = "Deletes a Cluster", notes = "Deletes a cluster.", response = String.class, tags = {
+            "EPM", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Creation successful", response = String.class),
+            @ApiResponse(code = 405, message = "Invalid input") })
+    @RequestMapping(value = "/epm/cluster/{clusterId}", produces = {
+            "application/json" }, consumes = {
+                    "application/json" }, method = RequestMethod.DELETE)
+    ResponseEntity<String> deleteCluster(
+            @ApiParam(value = "Id of a Cluster.", required = true) @PathVariable("clusterId") String clusterId);
+
+    @ApiOperation(value = "Returns all clusters", notes = "Returns all clusters.", response = Cluster.class, responseContainer = "List", tags = {
+            "EPM", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful operation", response = Cluster.class, responseContainer = "List"),
+            @ApiResponse(code = 404, message = "Resource not found") })
+    @RequestMapping(value = "/epm/cluster", produces = {
+            "application/json" }, method = RequestMethod.GET)
+    ResponseEntity<List<Cluster>> getAllClusters();
+
+    @ApiOperation(value = "Returns a cluster", notes = "Returns a cluster.", response = Cluster.class, tags = {
+            "EPM", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful operation", response = Cluster.class),
+            @ApiResponse(code = 404, message = "Resource not found") })
+    @RequestMapping(value = "/epm/cluster/{clusterId}", produces = {
+            "application/json" }, method = RequestMethod.GET)
+    ResponseEntity<Cluster> getCluster(
+            @ApiParam(value = "Id of a Cluster.", required = true) @PathVariable("clusterId") String clusterId);
+
+    /* ***************************************** */
+    /* ***************** Nodes ***************** */
+    /* ***************************************** */
+
+    @ApiOperation(value = "Creates a new node in the cluster specified", notes = "Creates a new node in the cluster specified.", response = String.class, tags = {
+            "EPM", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Creation successful", response = String.class),
+            @ApiResponse(code = 405, message = "Invalid input") })
+    @RequestMapping(value = "/epm/cluster/{clusterId}/node", produces = {
+            "application/json" }, consumes = {
+                    "application/json" }, method = RequestMethod.POST)
+    ResponseEntity<String> createNode(
+            @ApiParam(value = "Id of a Cluster.", required = true) @PathVariable("clusterId") String clusterId);
+
+    @ApiOperation(value = "Creates a new node in the cluster specified", notes = "Creates a new node in the cluster specified.", response = Cluster.class, tags = {
+            "EPM", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Creation successful", response = Cluster.class),
+            @ApiResponse(code = 405, message = "Invalid input") })
+    @RequestMapping(value = "/epm/cluster/{clusterId}/node/{nodeId}", produces = {
+            "application/json" }, consumes = {
+                    "application/json" }, method = RequestMethod.DELETE)
+    ResponseEntity<Cluster> deleteNode(
+            @ApiParam(value = "Id of a cluster.", required = true) @PathVariable("clusterId") String clusterId,
+            @ApiParam(value = "Id of a node.", required = true) @PathVariable("nodeId") String nodeId);
+
+    @ApiOperation(value = "Returns all cluster nodes", notes = "Returns all cluster nodes.", response = Worker.class, responseContainer = "List", tags = {
+            "EPM", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful operation", response = Worker.class, responseContainer = "List"),
+            @ApiResponse(code = 404, message = "Resource not found") })
+    @RequestMapping(value = "/epm/cluster/{clusterId}/node", produces = {
+            "application/json" }, method = RequestMethod.GET)
+    ResponseEntity<List<Worker>> getAllClusterNodes(
+            @ApiParam(value = "Id of a cluster.", required = true) @PathVariable("clusterId") String clusterId);
+
+    @ApiOperation(value = "Returns a cluster node", notes = "Returns a cluster node.", response = Worker.class, tags = {
+            "EPM", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful operation", response = Worker.class),
+            @ApiResponse(code = 404, message = "Resource not found") })
+    @RequestMapping(value = "/epm/cluster/{clusterId}/node/{nodeId}", produces = {
+            "application/json" }, method = RequestMethod.GET)
+    ResponseEntity<Worker> getClusterNode(
+            @ApiParam(value = "Id of a cluster.", required = true) @PathVariable("clusterId") String clusterId,
+            @ApiParam(value = "Id of a node.", required = true) @PathVariable("nodeId") String nodeId);
 
 }
--- elastest-torm/src/main/java/io/elastest/etm/api/EpmApiController.java
+++ elastest-torm/src/main/java/io/elastest/etm/api/EpmApiController.java
@@ -2,6 +2,8 @@ package io.elastest.etm.api;
 
 import static org.apache.commons.lang.SystemUtils.IS_OS_WINDOWS;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
@@ -11,10 +13,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.elastest.epm.client.model.Cluster;
+import io.elastest.epm.client.model.Worker;
+import io.elastest.epm.client.service.EpmClusterService;
 import io.elastest.etm.utils.EtmFilesService;
+import io.swagger.annotations.ApiParam;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-05-19T13:25:11.074+02:00")
 
@@ -25,6 +32,9 @@ public class EpmApiController implements EpmApi {
             .getLogger(EpmApiController.class);
     @Autowired
     EtmFilesService etmFilesService;
+
+    @Autowired
+    EpmClusterService epmClusterService;
 
     private static final String CLUSTER_TAR_FILE_NAME = "ansible-cluster.tar";
     private static final String NODE_TAR_FILE_NAME = "ansible-node.tar";
@@ -73,6 +83,120 @@ public class EpmApiController implements EpmApi {
             logger.error("Error on upload node tar file");
             return new ResponseEntity<Boolean>(false,
                     HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /* ******************************************* */
+    /* ***************** Cluster ***************** */
+    /* ******************************************* */
+
+    @Override
+    public ResponseEntity<Cluster> createCluster() {
+        try {
+            Cluster cluster = epmClusterService.createCluster();
+            return new ResponseEntity<Cluster>(cluster, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error on create Cluster", e);
+            return new ResponseEntity<Cluster>(
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ResponseEntity<String> deleteCluster(
+            @ApiParam(value = "Id of a Cluster.", required = true) @PathVariable("clusterId") String clusterId) {
+        try {
+            String responseString = epmClusterService.deleteCluster(clusterId);
+            return new ResponseEntity<String>(responseString, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error on delete Cluster {}", clusterId, e);
+            return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ResponseEntity<List<Cluster>> getAllClusters() {
+        try {
+            List<Cluster> clusters = epmClusterService.getAllClusters();
+            return new ResponseEntity<List<Cluster>>(clusters, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error on get all clusters {}", e);
+            return new ResponseEntity<List<Cluster>>(
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ResponseEntity<Cluster> getCluster(
+            @ApiParam(value = "Id of a Cluster.", required = true) @PathVariable("clusterId") String clusterId) {
+        try {
+            Cluster cluster = epmClusterService.getCluster(clusterId);
+            return new ResponseEntity<Cluster>(cluster, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error on get Cluster {}", clusterId, e);
+            return new ResponseEntity<Cluster>(
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /* ***************************************** */
+    /* ***************** Nodes ***************** */
+    /* ***************************************** */
+
+    @Override
+    public ResponseEntity<String> createNode(
+            @ApiParam(value = "Id of a Cluster.", required = true) @PathVariable("clusterId") String clusterId) {
+        try {
+            String responseString = epmClusterService
+                    .createNodeWorker(clusterId);
+            return new ResponseEntity<String>(responseString, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error on create node in cluster {}", clusterId, e);
+            return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ResponseEntity<Cluster> deleteNode(
+            @ApiParam(value = "Id of a cluster.", required = true) @PathVariable("clusterId") String clusterId,
+            @ApiParam(value = "Id of a node.", required = true) @PathVariable("nodeId") String nodeId) {
+        try {
+            Cluster cluster = epmClusterService.removeNode(clusterId, nodeId);
+            return new ResponseEntity<Cluster>(cluster, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error on delete node {} in Cluster {}", nodeId,
+                    clusterId, e);
+            return new ResponseEntity<Cluster>(
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ResponseEntity<List<Worker>> getAllClusterNodes(
+            @ApiParam(value = "Id of a cluster.", required = true) @PathVariable("clusterId") String clusterId) {
+        try {
+            List<Worker> clusterNodes = epmClusterService
+                    .getAllClusterNodes(clusterId);
+            return new ResponseEntity<List<Worker>>(clusterNodes,
+                    HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error on get all nodes of cluster {}", clusterId, e);
+            return new ResponseEntity<List<Worker>>(
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ResponseEntity<Worker> getClusterNode(
+            @ApiParam(value = "Id of a cluster.", required = true) @PathVariable("clusterId") String clusterId,
+            @ApiParam(value = "Id of a node.", required = true) @PathVariable("nodeId") String nodeId) {
+        try {
+            Worker node = epmClusterService.getClusterNode(clusterId, nodeId);
+            return new ResponseEntity<Worker>(node, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error on get node {} in Cluster {}", nodeId,
+                    clusterId, e);
+            return new ResponseEntity<Worker>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
