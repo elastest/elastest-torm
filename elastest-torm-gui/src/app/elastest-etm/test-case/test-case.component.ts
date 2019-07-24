5b00e755c4ae7dcabd4cd30decf9df4acce8ac02
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-07-24 10:25:59 +0200

    GUI improvements => fit test case video to parent and improve Files tab

--- elastest-torm-gui/src/app/elastest-etm/test-case/test-case.component.ts
+++ elastest-torm-gui/src/app/elastest-etm/test-case/test-case.component.ts
@@ -1,5 +1,5 @@
 import { TitlesService } from '../../shared/services/titles.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { TestCaseService } from './test-case.service';
 import { TestCaseModel } from './test-case-model';
@@ -7,10 +7,10 @@ import { ElastestLogAnalyzerComponent } from '../../elastest-log-analyzer/elaste
 import { TJobExecService } from '../tjob-exec/tjobExec.service';
 import { FileModel } from '../files-manager/file-model';
 import { ConfigurationService } from '../../config/configuration-service.service';
-import { MonitoringService } from '../../shared/services/monitoring.service';
 import { EtmMonitoringViewComponent } from '../etm-monitoring-view/etm-monitoring-view.component';
 import { TJobExecModel } from '../tjob-exec/tjobExec-model';
 import { StartFinishTestCaseTraces } from '../../elastest-log-analyzer/log-analyzer.service';
+import { ITdDataTableColumn } from '@covalent/core';
 
 @Component({
   selector: 'etm-test-case',
@@ -22,90 +22,118 @@ export class TestCaseComponent implements OnInit {
 
   @ViewChild('logsAndMetrics')
   logsAndMetrics: EtmMonitoringViewComponent;
-  showLogsAndMetrics: boolean = false;
 
-  public params;
-  public testCase: TestCaseModel;
-  public selectedTab: number;
+  showLogsAndMetrics: boolean = false;
 
   tJobId: number;
   tJobExecId: number;
+  testCaseId: number;
 
+  public selectedTab: number;
+
+  @Input()
   tJobExec: TJobExecModel;
+  @Input()
+  testCase: TestCaseModel;
+
+  nested: boolean = false;
 
   mp4Files: FileModel[] = [];
+
+  // SuT Data
+  filesColumns: ITdDataTableColumn[] = [
+    { name: 'name', label: 'Name' },
+    { name: 'options', label: 'Options', sortable: false, width: { min: 46, max: 130 } },
+  ];
 
   @ViewChild('miniLogAnalyzer') private miniLogAnalyzer: ElastestLogAnalyzerComponent;
 
   constructor(
     private testCaseService: TestCaseService,
-    private monitoringService: MonitoringService,
     public route: ActivatedRoute,
     private titlesService: TitlesService,
     private router: Router,
     private tJobExecService: TJobExecService,
     private configurationService: ConfigurationService,
   ) {
-    this.filesUrlPrefix = configurationService.configModel.host.replace('4200', '8091');
+    this.filesUrlPrefix = this.configurationService.configModel.host.replace('4200', '8091');
   }
 
   ngOnInit(): void {
     this.titlesService.setPathName(this.router.routerState.snapshot.url);
 
-    if (this.route.params !== null || this.route.params !== undefined) {
-      this.route.params.subscribe((params: Params) => {
-        this.params = params;
-        if (this.params) {
-          this.tJobId = this.params.tJobId;
-          this.tJobExecId = this.params.tJobExecId;
-          this.loadTestCase();
-        }
-      });
+    // Nested
+    if (this.tJobExec && this.testCase) {
+      this.nested = true;
+      this.tJobId = this.tJobExec.tJob.id;
+      this.tJobExecId = this.tJobExec.id;
+      this.testCaseId = this.testCase.id;
+      this.loadTestCase();
+    } else {
+      // Complete Section
+      if (this.route.params !== null || this.route.params !== undefined) {
+        this.route.params.subscribe((params: Params) => {
+          params = params;
+          if (params) {
+            this.tJobId = params.tJobId;
+            this.tJobExecId = params.tJobExecId;
+            this.testCaseId = params.testCaseId;
+            this.loadTestCase();
+          }
+        });
+      }
     }
   }
 
   loadTestCase(): void {
-    this.testCaseService.getTestCaseById(this.params.testCaseId).subscribe((testCase: TestCaseModel) => {
+    this.testCaseService.getTestCaseById(this.testCaseId).subscribe((testCase: TestCaseModel) => {
       this.testCase = testCase;
       this.testCase['result'] = this.testCase.getResultIcon();
       this.getExecutionFiles();
 
       if (this.logsAndMetrics) {
-        this.tJobExecService.getTJobExecutionByTJobId(this.tJobId, this.tJobExecId).subscribe(
-          (tJobExec: TJobExecModel) => {
-            this.tJobExec = tJobExec;
-
-            this.tJobExecService.logAnalyzerService
-              .searchTestCaseStartAndFinishTraces(
-                this.testCase.name,
-                [tJobExec.monitoringIndex],
-                tJobExec.startDate,
-                tJobExec.endDate,
-              )
-              .subscribe((startFinishObj: StartFinishTestCaseTraces) => {
-                if (startFinishObj) {
-                  let startDate: Date = startFinishObj.startDate;
-                  let endDate: Date = startFinishObj.finishDate;
-                  if (startDate && endDate) {
-                    this.logsAndMetrics.initView(tJobExec.tJob, tJobExec, startDate, endDate);
-                    this.showLogsAndMetrics = true;
-                  }
-                }
-              });
-          },
-          (error: Error) => console.error(error),
-        );
+        if (this.nested) {
+          this.initAfterLoadTJobExec();
+        } else {
+          this.tJobExecService.getTJobExecutionByTJobId(this.tJobId, this.tJobExecId).subscribe(
+            (tJobExec: TJobExecModel) => {
+              this.tJobExec = tJobExec;
+              this.initAfterLoadTJobExec();
+            },
+            (error: Error) => console.error(error),
+          );
+        }
       }
     });
   }
 
+  initAfterLoadTJobExec(): void {
+    this.tJobExecService.logAnalyzerService
+      .searchTestCaseStartAndFinishTraces(
+        this.testCase.name,
+        [this.tJobExec.monitoringIndex],
+        this.tJobExec.startDate,
+        this.tJobExec.endDate,
+      )
+      .subscribe((startFinishObj: StartFinishTestCaseTraces) => {
+        if (startFinishObj) {
+          let startDate: Date = startFinishObj.startDate;
+          let endDate: Date = startFinishObj.finishDate;
+          if (startDate && endDate) {
+            this.logsAndMetrics.initView(this.tJobExec.tJob, this.tJobExec, startDate, endDate);
+            this.showLogsAndMetrics = true;
+          }
+        }
+      });
+  }
+
   getExecutionFiles(): void {
-    this.tJobExecService.getTJobExecutionFiles(this.params.tJobId, this.params.tJobExecId).subscribe(
+    this.tJobExecService.getTJobExecutionFiles(this.tJobId, this.tJobExecId).subscribe(
       (tJobsExecFiles: FileModel[]) => {
         let i: number = 0;
         tJobsExecFiles.forEach((file: FileModel) => {
           if (this.isMP4(file)) {
-            file['tabRef'] = this.miniLogAnalyzer.componentsTree.treeModel.nodes.length + 2 + i; // components and Logs + Files tabs
+            file['order'] = i;
             i++;
           }
         });
@@ -126,28 +154,41 @@ export class TestCaseComponent implements OnInit {
     return file && file.name.endsWith('mp4');
   }
 
+  getVideoFileUrl(file: FileModel): string {
+    return this.filesUrlPrefix + file.encodedUrl;
+  }
+
+  openUrlInNewTab(file: FileModel): void {
+    window.open(this.getVideoFileUrl(file));
+  }
+
   goToTab(num: number): void {
     this.selectedTab = num;
   }
 
+  goToVideoTab(file: FileModel): void {
+    // video position, components Logs, ALL logs + Files tabs);
+    this.goToTab(file['order'] + this.miniLogAnalyzer.componentsTree.treeModel.nodes.length + 2);
+  }
+
   viewInLogAnalyzer(): void {
-    if (this.testCase && this.params) {
+    if (this.testCase) {
       this.router.navigate(
         [
           '/projects',
-          this.params.projectId,
+          this.tJobExec.tJob.project.id,
           'tjob',
-          this.params.tJobId,
+          this.tJobId,
           'tjob-exec',
-          this.params.tJobExecId,
+          this.tJobExecId,
           'testSuite',
-          this.params.testSuiteId,
+          this.testCase.testSuite.id,
           'testCase',
           this.testCase.id,
           'loganalyzer',
         ],
         {
-          queryParams: { tjob: this.params.tJobId, exec: this.params.tJobExecId, testCase: this.testCase.name },
+          queryParams: { tjob: this.tJobId, exec: this.tJobExecId, testCase: this.testCase.name },
         },
       );
     }
